bb9231f9ab8a1994e05dadae92a2d4a0f0e4980d
Jonas Roquette <roquettejonas@gmail.com>
2020-07-02 15:48:05 +0200

    Added explanatory comments

--- source/clock.cpp
+++ source/clock.cpp
@@ -17,14 +17,19 @@ int main()
         if (win.get_key(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             win.close();
         }
+        //Set Color, Center and initialize Circle with those parameters
         Color black{0, 0, 0};
         Vec2 ctr{400, 400};
         Circle clock{ctr, 100, black};
 
+        //Draw Clock
         clock.draw(win, 0.1f, 1);
+        
+        //Draw Clock Hands
         clock.drawSec(win);
         clock.drawMin(win);
         clock.drawHr(win);
+        
         
         win.update();
     }

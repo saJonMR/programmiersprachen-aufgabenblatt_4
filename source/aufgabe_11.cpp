c05c21e40a757f2d8ca4aebc3e3b77efcd057d4e
Jonas Roquette <roquettejonas@gmail.com>
2020-07-02 15:52:29 +0200

    Copied Assignment structure

--- source/aufgabe_11.cpp
+++ source/aufgabe_11.cpp
@@ -7,6 +7,7 @@ TEST_CASE("vectoren inhalte addieren", "[add]") {
     std::vector<int> v_1{1, 2, 3, 4, 5, 6, 7, 8, 9};
     std::vector<int> v_2{9, 8, 7, 6, 5, 4, 3, 2, 1};
     std::vector<int> v_3{};
+    //
 
     //REQUIRE(std::all_of(v_3.begin(), v_3.end(), XXXX));
 }
